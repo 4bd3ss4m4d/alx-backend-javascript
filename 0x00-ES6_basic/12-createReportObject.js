0d05c1b5a6bb90fde7b36fc962c8949c5203e6e8
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-04-15 02:46:09 +0100

    12-createReportObject.js Fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,10 @@
 export default function createReportObject(employeesList) {
-  const getNumberOfDepartments = () => Object.keys(employeesList).length;
-  return { allEmployees: employeesList, getNumberOfDepartments };
+  return {
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+    allEmployees: {
+      ...employeesList,
+    },
+  };
 }
