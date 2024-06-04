605ddde5225c3fe9c8a7b2f27ac545b59e2879d0
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-06-04 19:09:51 +0100

    Fix Task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,49 +1,47 @@
-const http = require("http");
-const fs = require("fs").promises;
+const http = require('http');
+const fs = require('fs').promises;
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8")
+    fs.readFile(path, 'utf8')
       .then((data) => {
-        const lns = data.split("\n");
-        const hashT = {};
-        let allStudents = -1;
-        let finalResult = "";
-        for (const line of lns) {
-          if (line.trim() !== "") {
-            const cols = line.split(",");
-            const fld = cols[3];
-            const firstName = cols[0];
-            if (allStudents >= 0) {
-              if (!Object.hasOwnProperty.call(hashtable, fld)) {
-                hashtable[fld] = [];
+        const lines = data.split('\n');
+        const hashtable = {};
+        let students = -1;
+        let result = '';
+        for (const line of lines) {
+          if (line.trim() !== '') {
+            const columns = line.split(',');
+            const field = columns[3];
+            const firstname = columns[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
               }
-              hashtable[fld] = [...hashtable[fld], firstName];
+              hashtable[field] = [...hashtable[field], firstname];
             }
-            allStudents += 1;
+            students += 1;
           }
         }
-        finalResult += `Number of students: ${allStudents}\n`;
+        result += `Number of students: ${students}\n`;
         for (const key in hashtable) {
           if (Object.hasOwnProperty.call(hashtable, key)) {
-            finalResult += `Number of students in ${key}: ${
-              hashtable[key].length
-            }. List: ${hashtable[key].join(", ")}\n`;
+            result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
           }
         }
-        resolve(finalResult);
+        resolve(result);
       })
       .catch(() => {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       });
   });
 }
 
 const app = http.createServer((req, res) => {
-  if (req.url === "/") {
+  if (req.url === '/') {
     res.writeHead(200);
-    res.end("Hello Holberton School!");
-  } else if (req.url === "/students") {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     countStudents(process.argv[2])
       .then((data) => {
         res.writeHead(200);
@@ -55,7 +53,7 @@ const app = http.createServer((req, res) => {
       });
   } else {
     res.writeHead(404);
-    res.end("Not foundx");
+    res.end('Not foundx');
   }
 });
 
